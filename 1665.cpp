7251ac7e8021af79bdf7ec1ead6b31c826c175a8
Jyothi <143396404+JYOTHIkudipudi@users.noreply.github.com>
2026-05-12 22:09:02 +0530

    Update 1665.cpp

--- 1665.cpp
+++ 1665.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
    
     int minimumEffort(vector<vector<int>>& tasks) {
-
+   
         sort(tasks.begin(), tasks.end(),
             [](vector<int>& a, vector<int>& b) {
                 return (a[1] - a[0]) > (b[1] - b[0]);
