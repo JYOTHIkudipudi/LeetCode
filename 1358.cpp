1af721dfe8a32aec4e7581dc81e8ace3c536bba1
Jyothi <143396404+JYOTHIkudipudi@users.noreply.github.com>
2026-06-30 20:30:46 +0530

    Create 1358.cpp

---
+++ 1358.cpp
@@ -0,0 +1,53 @@
+/*  1358. Number of Substrings Containing All Three Characters
+
+Given a string s consisting only of characters a, b and c.
+
+Return the number of substrings containing at least one occurrence of all these characters a, b and c.
+
+ 
+
+Example 1:
+
+Input: s = "abcabc"
+Output: 10
+Explanation: The substrings containing at least one occurrence of the characters a, b and c are "abc", "abca", "abcab", "abcabc", "bca", "bcab", "bcabc", "cab", "cabc" and "abc" (again). 
+Example 2:
+
+Input: s = "aaacb"
+Output: 3
+Explanation: The substrings containing at least one occurrence of the characters a, b and c are "aaacb", "aacb" and "acb". 
+Example 3:
+
+Input: s = "abc"
+Output: 1
+ 
+
+Constraints:
+
+3 <= s.length <= 5 x 10^4
+s only consists of a, b or c characters.  */
+
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int n = s.size();
+
+        vector<int> freq(3, 0);
+
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < n; right++) {
+            freq[s[right] - 'a']++;
+
+            while (freq[0] > 0 && freq[1] > 0 && freq[2] > 0) {
+                ans += (n - right);
+
+                freq[s[left] - 'a']--;
+                left++;
+            }
+        }
+
+        return ans;
+    }
+};
